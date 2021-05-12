d47fbaf0b96b185d91696a92a9841e5db21dc111
jeongtai <41337277+jeongtai@users.noreply.github.com>
2021-05-12 18:57:37 +0900

    #3.9 Getting the Weather part Two API

--- todo.js
+++ todo.js
@@ -1,3 +1,7 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+	toDoInput = toDoForm.querySelector("input"),
+	toDoList = document.querySelector(".js-toDoList");
+
 const TODOS_LS = "toDos";
 
 let toDos = [];
@@ -5,7 +9,6 @@ let toDos = [];
 function deleteToDo(event) {
 	const btn = event.target;
 	const li = btn.parentNode;
-
 	toDoList.removeChild(li);
 	const cleanToDos = toDos.filter(function (toDo) {
 		return toDo.id !== parseInt(li.id);
@@ -17,6 +20,7 @@ function deleteToDo(event) {
 function saveToDos() {
 	localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
+
 function paintToDo(text) {
 	const li = document.createElement("li");
 	const delBtn = document.createElement("button");
@@ -36,12 +40,14 @@ function paintToDo(text) {
 	toDos.push(toDoObj);
 	saveToDos();
 }
+
 function handleSubmit(event) {
 	event.preventDefault();
 	const currentValue = toDoInput.value;
 	paintToDo(currentValue);
 	toDoInput.value = "";
 }
+
 function loadToDos() {
 	const loadedToDos = localStorage.getItem(TODOS_LS);
 	if (loadedToDos !== null) {
@@ -51,8 +57,10 @@ function loadToDos() {
 		});
 	}
 }
+
 function init() {
 	loadToDos();
 	toDoForm.addEventListener("submit", handleSubmit);
 }
+
 init();
--- weather.js
+++ weather.js
@@ -1,4 +1,19 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "608ae81a1b13deded36f667e189b6bb7";
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (json) {
+			const temperature = json.main.temp;
+			const place = json.name;
+			weather.innerText = `${temperature} @ ${place}`;
+		});
+}
 
 function saveCoords(coordsObj) {
 	localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +27,7 @@ function handleGeoSucces(position) {
 		longitude
 	};
 	saveCoords(coordsObj);
+	getWeather(latitude, longitude);
 }
 
 function hadnleGeoError(position) {
@@ -22,11 +38,12 @@ function askForCoords() {
 	navigator.geolocation.getCurrentPosition(handleGeoSucces, hadnleGeoError);
 }
 function loadCoords() {
-	const loadedCords = localStorage.getItem(COORDS);
-	if (loadedCords === null) {
+	const loadedCoords = localStorage.getItem(COORDS);
+	if (loadedCoords === null) {
 		askForCoords();
 	} else {
-		// getWeather
+		const parsedCoords = JSON.parse(loadedCoords);
+		getWeather(parsedCoords.latitude, parsedCoords.longitude);;
 	}
 }
 
