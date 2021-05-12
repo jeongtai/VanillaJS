a2ded4f9c41248b51961459d253d6ad0756d58f2
jeongtai <41337277+jeongtai@users.noreply.github.com>
2021-05-12 16:48:31 +0900

    #3.5 Making a To Do List part Two

--- todo.js
+++ todo.js
@@ -4,16 +4,30 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos() {
+	//JSON.stringify는 js의 object를 string으로 바꿔줌
+	localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
 	const li = document.createElement("li");
 	const delBtn = document.createElement("button");
-	delBtn.innerText = "❌";
 	const span = document.createElement("span");
+	const newId = toDos.length + 1;
+	delBtn.innerText = "❌";
 	span.innerText = text;
 	li.appendChild(delBtn);
 	li.appendChild(span);
+	li.id = newId;
 	toDoList.appendChild(li);
-
+	const toDoObj = {
+		text: text,
+		id: newId
+	};
+	toDos.push(toDoObj);
+	saveToDos();
 }
 
 function handleSubmit(event) {
@@ -21,20 +35,20 @@ function handleSubmit(event) {
 	const currentValue = toDoInput.value;
 	paintToDo(currentValue);
 	toDoInput.value = "";
-
 }
 
 function loadToDos() {
-	const toDos = localStorage.getItem(TODOS_LS);
-	if (toDos !== null) {
+	const loadedToDos = localStorage.getItem(TODOS_LS);
+	if (loadedToDos !== null) {
+		const parsedToDos = JSON.parse(loadedToDos);
+		parsedToDos.forEach(function (toDo) {
+			paintToDo(toDo.text);
+		});
 	}
 }
 
 function init() {
 	loadToDos();
 	toDoForm.addEventListener("submit", handleSubmit);
-
 }
-
-
 init();
